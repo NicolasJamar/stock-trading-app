d785547ac01e951adc07905db5f875ced49c4382
NicolasJamar <nicolas.jamar@gmail.com>
2023-02-07 17:48:52 +0100

    format date

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -1,6 +1,83 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import FinnHub from "../apis/FinnHub";
+
+
+const formatData = (data) => {
+  return data.t.map( (el, index) => {
+    return {
+     x: el * 1000, // time
+     y: data.c[index] // closure
+   }
+  })
+}
+
+
 function StockDetailPage() {
+  //To use the symbol in url
+  const {symbol} = useParams();
+  const [chartData, setChartData] = useState([]);
+
+  useEffect( () => {
+    const fetchData = async() => {
+
+      const date = new Date()
+      // convert time stamp to seconds
+      const currentTime = Math.floor(date.getTime()/1000)
+      let oneDay;
+      if(date.getDay() === 6) { //if Saturday
+        oneDay = currentTime - 2*60*60*24
+      } else if(date.getDay() === 0) { //if Sunday
+        oneDay = currentTime - 3*60*60*24
+      } else {
+        oneDay = currentTime - 60*60*24
+      }
+      const oneWeek = currentTime - 7*60*60*24
+      const oneYear = currentTime - 365*60*60*24
+
+      try {
+        const responses = await Promise.all([
+          FinnHub.get("/stock/candle", {
+            params: {
+              symbol,
+              resolution: 30,
+              from: oneDay,
+              to: currentTime
+            }
+          }),
+          FinnHub.get("/stock/candle", {
+            params: {
+              symbol,
+              resolution: 60,
+              from: oneWeek,
+              to: currentTime
+            }
+          }),
+          FinnHub.get("/stock/candle", {
+            params: {
+              symbol,
+              resolution: "W",
+              from: oneYear,
+              to: currentTime
+            }
+          })
+        ]);
+
+        setChartData({
+          day: formatData(responses[0].data),
+          week: formatData(responses[1].data),
+          year: formatData(responses[2].data)
+        })
+        
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return(
-    <div>Stock Detail Page</div>
+    <div>Stock Detail Page {symbol}</div>
   )
 }
 
